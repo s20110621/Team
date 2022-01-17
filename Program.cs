ff4ee27ea84b2b0d478dad4307ed304eb1cf2883
Marat Gylev <radzhab.pashaev@mail.ru>
2022-01-17 16:37:19 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = { 3,5,6,7,2,9 }; 
+
+void ArrayOfEvenNumbers (int[] array)
+
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] % 2 == 0) Console.Write($"{array[index]} ");
+    }
+}
+
+ArrayOfEvenNumbers(array);
